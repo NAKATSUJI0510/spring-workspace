82a08c29b8b5af5c443d14e784f989fc8db4d4f8
aiueo <aiueo@aaa.com>
2025-05-20 17:46:52 +0900

    0520 commit

---
+++ spring-chap8-ec/src/main/java/com/example/demo/entity/Item.java
@@ -0,0 +1,68 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "items")
+public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; // 商品ID
+
+	@Column(name = "category_id")
+	private Integer categoryId; // カテゴリーID
+
+	private String name; // 商品名
+
+	private Integer price; // 価格
+
+	@Transient // 永続化対象外
+	private Integer quantity; // 数量
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+}
